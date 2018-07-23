422dffdf153c302b2f5ae3b7e4c51e6b4f74c6ee
Brad Fitzpatrick <brad@danga.com>
2018-07-22 22:18:49 -0700

    Basically working, but needs cleanup.

--- resize.go
+++ resize.go
@@ -20,6 +20,7 @@ limitations under the License.
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
@@ -39,7 +40,11 @@ const (
 	rootx8664GPTTypeID = "4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709"
 )
 
-var dev = flag.String("dev", "", "device to enlarge; defaults to the only applicable disk if it's not ambiguous")
+var (
+	dev     = flag.String("dev", "", "device to enlarge; defaults to the only applicable disk if it's not ambiguous")
+	dry     = flag.Bool("dry-run", false, "don't make changes")
+	verbose = flag.Bool("verbose", false, "verbose output")
+)
 
 func main() {
 	flag.Parse()
@@ -56,7 +61,15 @@ func main() {
 	if !strings.Contains(*dev, "/") {
 		*dev = "/dev/" + *dev
 	}
-	fmt.Printf("getting partition table for %q ...\n", *dev)
+
+	partDev := enlargePartition()
+	vg := enlargeLVM(partDev)
+	enlargeFS(partDev, vg)
+
+	fmt.Println("Success.")
+}
+
+func enlargePartition() (partDev string) {
 	pt := getPartitionTable(*dev)
 	if len(pt.parts) == 0 {
 		log.Fatalf("device %q has no partitions", *dev)
@@ -72,15 +85,12 @@ func main() {
 	}
 
 	part := pt.parts[len(pt.parts)-1]
+	partDev = part.dev
 	lastType := part.Type()
 
-	var needPVExtend bool
 	if isGPT {
 		switch lastType {
-		case lvmGPTTypeID:
-			fmt.Println("final partition is LVM PV")
-			needPVExtend = true
-		case rootx8664GPTTypeID:
+		case lvmGPTTypeID, rootx8664GPTTypeID:
 		default:
 			log.Fatalf("unknown GPT partition type %q for %s", lastType, part.dev)
 		}
@@ -92,27 +102,230 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Partition table:\n")
-	pt.Write(os.Stdout)
-	fmt.Println()
+	if *verbose {
+		fmt.Printf("Current partition table:\n")
+		pt.Write(os.Stdout)
+		fmt.Println()
+	}
 
 	size := readInt64File("/sys/block/sda/size")
-	fmt.Printf("Cur size: %d\n", size)
-
-	fmt.Printf("Part start: %d\n", part.Start())
-	fmt.Printf("Part size: %d\n", part.Size())
 	end := part.Start() + part.Size()
-	fmt.Printf("Part end: %d\n", end)
 	remain := size - end
-	fmt.Printf("Remaining after final partition: %d\n", remain)
-	if remain <= 2048 {
-		fmt.Println("partition is at max size")
+	if *verbose {
+		fmt.Printf("Cur size: %d\n", size)
+		fmt.Printf("Part start: %d\n", part.Start())
+		fmt.Printf("Part size: %d\n", part.Size())
+		fmt.Printf("Part end: %d\n", end)
+		fmt.Printf("Remaining after final partition: %d\n", remain)
+	}
+	sectorSize := 512 // TODO: get from /sys/block/sda/queue/hw_sector_size
+	endReserve := int64(1<<20) / int64(sectorSize)
+	if remain <= endReserve {
+		fmt.Printf("Partition %s is at max size; no need to extend.\n", part.dev)
 		return
 	}
-	extend := size - end - 2048
+	extend := remain - endReserve
 	fmt.Printf("Need to extend disk by %d sectors (%d bytes, %0.03f GiB)\n", extend, extend*512, float64(extend)*512/(1<<30))
-	if needPVExtend {
-		fmt.Println("needs PV extend")
+
+	part.SetSize(part.Size() + extend)
+	if *verbose {
+		fmt.Printf("New partition table to write:\n")
+		pt.RemoveMeta("last-lba") // or sfdisk complains
+	}
+
+	var newPart bytes.Buffer
+	pt.Write(&newPart)
+	fmt.Printf("%s\n", newPart.Bytes())
+
+	if *dry {
+		fmt.Printf("[dry-run] would've run sfdisk -f to set new partition table\n")
+		return
+	}
+
+	fmt.Println("Setting new partition table...")
+	cmd := exec.Command("/sbin/sfdisk", "-f", "--no-reread", "--no-tell-kernel", *dev)
+	cmd.Stdin = bytes.NewReader(newPart.Bytes())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("sfdisk: %v", err)
+	}
+
+	if out, err := exec.Command("partprobe").Output(); err != nil {
+		if _, err := exec.LookPath("partprobe"); err != nil {
+			log.Fatalf("Program 'partprobe' not found; apt-get install parted ?")
+		}
+		log.Fatalf("partprobe: %v, %s", err, out)
+	}
+	return
+}
+
+func enlargeLVM(partDev string) (vg string) {
+	if !anyDeviceMapperDevs() {
+		fmt.Printf("No LVM LVs found; skipping PV resize\n")
+		return
+	}
+	out, err := exec.Command("pvdisplay", "-c").Output()
+	if err != nil {
+		log.Fatalf("pvdisplay: %v", err)
+	}
+	lines := strings.Split(string(out), "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		f := strings.Split(line, ":")
+		if len(f) < 2 || f[0] != partDev {
+			continue
+		}
+		vg = f[1]
+		if *dry {
+			fmt.Printf("[dry-run] would've run pvresize %s\n", partDev)
+			return
+		}
+		out, err := exec.Command("pvresize", partDev).Output()
+		if err != nil {
+			log.Fatalf("pvresize: %v, %s", err, err.(*exec.ExitError).Stderr)
+		}
+		fmt.Printf("LVM PV %s resized: %s\n", partDev, out)
+		return
+	}
+	fmt.Printf("No LVM PV found on %s; not resized\n", *dev)
+	return
+}
+
+func anyDeviceMapperDevs() bool {
+	fis, err := ioutil.ReadDir("/sys/block")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fi := range fis {
+		name := filepath.Base(fi.Name())
+		if strings.HasPrefix(name, "dm-") {
+			return true
+		}
+	}
+	return false
+}
+
+func enlargeFS(partDev, vg string) {
+	var devs []os.FileInfo
+
+	addDev := func(dev string) {
+		if fi, err := os.Stat(dev); err != nil {
+			log.Fatal(err)
+		} else {
+			devs = append(devs, fi)
+		}
+	}
+	devInSet := func(dev string) bool {
+		fi, err := os.Stat(dev)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, fib := range devs {
+			if os.SameFile(fi, fib) {
+				return true
+			}
+		}
+		return false
+	}
+
+	addDev(partDev)
+
+	// If there was an LVM volume group, find which LV device it's on.
+	if vg != "" {
+		out, err := exec.Command("lvdisplay", "-c").Output()
+		if err != nil {
+			log.Fatalf("lvdisplay: %v, %s", err, err.(*exec.ExitError).Stderr)
+		}
+		bs := bufio.NewScanner(bytes.NewReader(out))
+		for bs.Scan() {
+			f := strings.Split(strings.TrimSpace(bs.Text()), ":")
+			if len(f) < 2 || f[1] != vg {
+				continue
+			}
+			lvDev := f[0]
+			addDev(lvDev)
+			if *dry {
+				fmt.Println("[dry-run] not expanding LVM LV device %v\n", lvDev)
+			} else {
+				_, err := exec.Command("lvextend", "-l", "+100%FREE", lvDev).Output()
+				if err != nil {
+					errMsg := string(err.(*exec.ExitError).Stderr)
+					if strings.Contains(errMsg, "matches existing size") {
+						fmt.Printf("lvextend -l +100%%FREE %v: no result; already at max size\n", lvDev)
+					} else {
+						log.Fatalf("lvextend: %v, %s", err, errMsg)
+					}
+				} else {
+					fmt.Printf("ran lvextend -l +100%%FREE %v\n", lvDev)
+				}
+			}
+		}
+	}
+
+	type fileSystem struct {
+		dev    string
+		mount  string
+		fstype string
+	}
+	var fileSystems []fileSystem
+
+	mounts, err := ioutil.ReadFile("/proc/mounts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bs := bufio.NewScanner(bytes.NewReader(mounts))
+	for bs.Scan() {
+		f := strings.Fields(bs.Text())
+		if len(f) < 3 {
+			continue
+		}
+		dev := f[0]
+		if !strings.HasPrefix(dev, "/") {
+			continue
+		}
+		if devInSet(dev) {
+			fileSystems = append(fileSystems, fileSystem{
+				dev:    dev,
+				mount:  f[1],
+				fstype: f[2],
+			})
+		}
+	}
+	if len(fileSystems) == 0 {
+		fmt.Printf("no filesystem found on %s\n", *dev)
+		return
+	}
+	if len(fileSystems) > 1 {
+		log.Fatalf("stopping due to unexpected >1 filesystem found on %s: %+v", *dev, fileSystems)
+	}
+	fs := fileSystems[0]
+
+	var cmd *exec.Cmd
+	switch fs.fstype {
+	case "ext2", "ext3", "ext4":
+		cmd = exec.Command("resize2fs", fs.dev)
+	case "xfs":
+		cmd = exec.Command("xfs_growfs", "-d", fs.mount)
+	case "btrfs":
+		cmd = exec.Command("btrfs", "filesystem", "resize", "max", fs.mount)
+	default:
+		log.Fatalf("don't know how to resize filesystem type for %+v", fs)
+	}
+	if *dry {
+		fmt.Printf("[dry-run] would have run %s %v", cmd.Path, cmd.Args)
+		return
+	}
+	fmt.Printf("Enlarging %+v with %s %v ...\n", fs, cmd.Path, cmd.Args)
+	out, err := cmd.CombinedOutput()
+	outs := string(out)
+	if strings.Contains(outs, "Nothing to do!") {
+		fmt.Println("... nothing to do.")
+	} else {
+		fmt.Println(outs)
+	}
+	if err != nil {
+		log.Fatalf("%s: %v", cmd.Path, err)
 	}
 }
 
@@ -130,6 +343,11 @@ func readInt64File(f string) int64 {
 }
 
 /*
+
+TODO: verify 512 or do the right thing with different sizes
+$ cat /sys/block/sda/queue/hw_sector_size
+512
+
 can be GPT:
 
 label: gpt
@@ -171,6 +389,18 @@ func (pt *partitionTable) Meta(k string) string {
 	return ""
 }
 
+func (pt *partitionTable) RemoveMeta(key string) {
+	var newMeta []string
+	for _, meta := range pt.meta {
+		if strings.HasPrefix(meta, key) &&
+			strings.HasPrefix(meta, key+": ") {
+			continue
+		}
+		newMeta = append(newMeta, meta)
+	}
+	pt.meta = newMeta
+}
+
 func (pt *partitionTable) Write(w io.Writer) error {
 	var buf bytes.Buffer
 	for _, meta := range pt.meta {
@@ -206,6 +436,16 @@ func (sl sfdiskLine) Attr(key string) string {
 		}
 	}
 	return ""
+}
+
+func (sl sfdiskLine) SetSize(size int64) {
+	for i, attr := range sl.attr {
+		if strings.HasPrefix(attr, "size=") {
+			sl.attr[i] = fmt.Sprintf("size=%d", size)
+			return
+		}
+	}
+	panic("didn't find size attribute")
 }
 
 func (sl sfdiskLine) AttrInt64(key string) int64 {
@@ -297,28 +537,6 @@ sda                   8:0    0 171798691840  0 disk
   └─debian--vg-root 254:0    0 160801226752  0 lvm  /
 
 
-root@debian:~# sfdisk -d /dev/sda
-label: gpt
-label-id: 86CA60D6-FE21-41E7-96AB-42BB6372BA35
-device: /dev/sda
-unit: sectors
-first-lba: 34
-last-lba: 10485726
-
-/dev/sda1 : start=        2048, size=     1048576, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, uuid=C9C1EF10-FE31-4B94-BDBC-C2750F8FE621
-/dev/sda2 : start=     1050624, size=      499712, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=1C0A64F2-A6CA-4798-92BE-6F37C5E1C55B
-/dev/sda3 : start=     1550336, size=     8933376, type=E6D6D379-F507-44C2-A23C-238F2A3DF928, uuid=183184BF-371C-4406-8B06-9B872F464487
-root@debian:~# cat /sys/block/sda/size
-10485760
-
->>> 1550336+8933376
-10483712
-
->>> 10485760-10483712
-2048
-
-
-
 root@kbase:~# sfdisk -d /dev/sda
 label: dos
 label-id: 0x877f0a6b
@@ -331,6 +549,6 @@ unit: sectors
 
 Deps:
 util-linux: /sbin/sfdisk
-parted:
+parted: partprobe
 
 */
